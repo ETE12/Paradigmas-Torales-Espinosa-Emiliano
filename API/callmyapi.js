005b7946100dd0b17d02262ee63aa5c6faa1dfb2
ETE12 <emilianotoes12@gmail.com>
2024-02-27 10:01:48 -0600

    callmyapi.html

--- API/callmyapi.js
+++ API/callmyapi.js
@@ -1,69 +1,103 @@
 function getMyIpHttp() {
- var request = new XMLHttpRequest()
+  var request = new XMLHttpRequest();
 
- request.open('GET', 'https://digimon-api.vercel.app/api/digimon', true)
- request.onload = function (data) {
-  console.log(data);
-  console.log(data.target.response);
- }
+  request.open('GET', 'https://digimon-api.vercel.app/api/digimon', true);
+  request.onload = function(data) {
+    var responseData = JSON.parse(data.target.response);
+    if (Array.isArray(responseData)) {
+      responseData.forEach(function(digimon) {
+        if (digimon.img) {
+          var imgElement = document.createElement('img');
+          imgElement.src = digimon.img;
+          document.body.appendChild(imgElement);
+        }
+      });
+    } else {
+      console.log(responseData);
+    }
+  };
 
-request.send()
+  request.send();
 }
 
 function getMyIpFetch(){
-  var request = new Request('https://digimon-api.vercel.app/api/digimon/name/agumon',
-   {
-     method: 'GET',
-  headers: new Headers({ 'Content-Type': 'application/json'})
+  var request = new Request('https://digimon-api.vercel.app/api/digimon/name/agumon', {
+    method: 'GET',
+    headers: new Headers({ 'Content-Type': 'application/json'})
+  });
+
+  fetch(request)
+    .then(function(response) {
+      return response.json();
+    })
+    .then(function(data) {
+      if (data[0].img) {
+        var imgElement = document.createElement('img');
+        imgElement.src = data[0].img;
+        document.body.appendChild(imgElement);
+      } else {
+        console.log(data[0]);
+      }
+    })
+    .catch(function(err) {
+      console.log(err);
     });
-
-
-   fetch(request)
-   .then(function(response) {
-      console.log(response);
-      response.json().then(function(respjson){	      
-        console.log (respjson[0]);
-  });
- })
- .catch(err => {
-        console.log(err);
-  });
 }
 
 function getMyIpCDN() {
- $.getJSON("https://digimon-api.vercel.app/api/digimon",
-   function(json) {
-     console.log(json);
-	   $('#resultado').text(JSON.stringify(json));
-       }
-      );
+  $.getJSON("https://digimon-api.vercel.app/api/digimon", function(json) {
+    if (Array.isArray(json)) {
+      json.forEach(function(digimon) {
+        if (digimon.img) {
+          var imgElement = document.createElement('img');
+          imgElement.src = digimon.img;
+          document.body.appendChild(imgElement);
+        }
+      });
+    } else {
+      $('#resultado').text(JSON.stringify(json));
+    }
+  });
 }
 
 function getMyIpDataCDN(){
-  $.getJSON("https://digimon-api.vercel.app/api/digimon/name/agumon",
-    function(json) {
-     console.log(json[0]);
+  $.getJSON("https://digimon-api.vercel.app/api/digimon/name/agumon", function(json) {
+    if (json[0].img) {
+      var imgElement = document.createElement('img');
+      imgElement.src = json[0].img;
+      document.body.appendChild(imgElement);
+    } else {
+      console.log(json[0]);
     }
-  );
+  });
 }
 
-
 function getMyIpLocal() {
- $.getJSON("https://digimon-api.vercel.app/api/digimon",
-   function(json) {
-     console.log(json);
-	     $('#resultado').text(JSON.stringify(json));
-
-       }
-      );
+  $.getJSON("https://digimon-api.vercel.app/api/digimon", function(json) {
+    if (Array.isArray(json)) {
+      json.forEach(function(digimon) {
+        if (digimon.img) {
+          var imgElement = document.createElement('img');
+          imgElement.src = digimon.img;
+          document.body.appendChild(imgElement);
+        }
+      });
+    } else {
+      $('#resultado').text(JSON.stringify(json));
+    }
+  });
 }
 
 function getMyDataLocal(){
-  $.getJSON("https://digimon-api.vercel.app/api/digimon/name/agumon",
-    function(json) {
-     console.log(json[0]);
+  $.getJSON("https://digimon-api.vercel.app/api/digimon/name/agumon", function(json) {
+    if (json[0].img) {
+      var imgElement = document.createElement('img');
+      imgElement.src = json[0].img;
+      document.body.appendChild(imgElement);
+    } else {
+      console.log(json[0]);
     }
-  );
+  });
 }
 
 
